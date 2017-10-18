2126bb4417c9d9c25ba87107e3b8eb7762bc3e2c
Tim Lovorn <tflovorn@austin.utexas.edu>
2017-10-18 12:51:15 -0500

    add orbital dos plot

--- plot/plot_dos.py
+++ plot/plot_dos.py
@@ -14,6 +14,55 @@ def find_with_prefix(prefix):
 
     return with_prefix
 
+def plot_total_dos(dos_data, full_prefix):
+    es = dos_data["es"]
+    total_dos = dos_data["total_dos"]
+
+    if "xlabel" in dos_data:
+        plt.xlabel(dos_data["xlabel"])
+
+    if "ylabel" in dos_data:
+        plt.ylabel(dos_data["ylabel"])
+
+    if "caption" in dos_data:
+        plt.title(dos_data["caption"])
+
+    plot_path = "{}_dos.png".format(full_prefix)
+
+    plt.xlim(min(es), max(es))
+    plt.ylim(0.0, max(total_dos))
+
+    plt.plot(es, total_dos, 'k-')
+
+    plt.savefig(plot_path, dpi=500, bbox_inches='tight')
+    plt.clf()
+
+def plot_orbital_dos(dos_data, full_prefix):
+    es = dos_data["es"]
+    orbital_dos = dos_data["orbital_dos"]
+
+    if "xlabel" in dos_data:
+        plt.xlabel(dos_data["xlabel"])
+
+    if "ylabel" in dos_data:
+        plt.ylabel(dos_data["ylabel"])
+
+    if "caption" in dos_data:
+        plt.title(dos_data["caption"])
+
+    plot_path = "{}_orbital_dos.png".format(full_prefix)
+
+    plt.xlim(min(es), max(es))
+    plt.ylim(0.0, max([max(orb) for orb in orbital_dos]))
+
+    for orb_index, orb in enumerate(orbital_dos):
+        plt.plot(es, orb, label="Orbital {}".format(orb_index + 1))
+
+    plt.legend(loc=0)
+
+    plt.savefig(plot_path, dpi=500, bbox_inches='tight')
+    plt.clf()
+
 def _main():
     parser = argparse.ArgumentParser("Plot total DOS")
     parser.add_argument("prefix", type=str, help="Calculation prefix")
@@ -25,27 +74,8 @@ def _main():
         with open(dos_path) as fp:
             dos_data = json.load(fp)
 
-        es = dos_data["es"]
-        total_dos = dos_data["total_dos"]
-
-        if "xlabel" in dos_data:
-            plt.xlabel(dos_data["xlabel"])
-
-        if "ylabel" in dos_data:
-            plt.ylabel(dos_data["ylabel"])
-
-        if "caption" in dos_data:
-            plt.title(dos_data["caption"])
-
-        plot_path = "{}_dos.png".format(full_prefix)
-
-        plt.xlim(min(es), max(es))
-        plt.ylim(0.0, max(total_dos))
-
-        plt.plot(es, total_dos, 'k-')
-
-        plt.savefig(plot_path, dpi=500, bbox_inches='tight')
-        plt.clf()
+        plot_total_dos(dos_data, full_prefix)
+        plot_orbital_dos(dos_data, full_prefix)
 
 if __name__ == "__main__":
     _main()
